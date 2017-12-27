047121a2de940e3adf3791db2977834508e3bd96
fantasygamery <30944486+fantasygamery@users.noreply.github.com>
2017-12-27 14:54:34 -0500

    Update Main.js

--- Src/Main.js
+++ Src/Main.js
@@ -1,35 +1,34 @@
-
  nnFunc=function(){
   //FOREACH
   this.each=function(_object,_function){
   for (var x=0;x<_object.length;x++){
   _function(_object[x],'Sup?');
   }
-  }
+  };
 // WAIT
   this.wait=function (secs,func,ID){
 var _towait=secs;var waited=0;
-var INsT = null
+var INsT = null;
 function _int(){
-    waited=++waited
+    waited=++waited;
 if (ID){
-document.getElementById(ID).innerHTML=_towait-waited
+document.getElementById(ID).innerHTML=_towait-waited;
 }
     if(waited >= _towait){
-     if (func !== null || func  !== undefined ){
+     if (func !== null && func  !== undefined ){
 try {
 func();
 } catch( __ERROR ){console.log(__ERROR.message)}}
-window.clearInterval(INsT)
+window.clearInterval(INsT);
 
 }
 }
 INsT=window.setInterval(_int,1000);
-}  
+}; 
   
   // RETURN THIS FUNCTION
-  return this
-  }
+  return this;
+  };
  window.nn=nnFunc();
 // SET STATUS , TO FIRE LOADER
   window.nnReady='yes';
