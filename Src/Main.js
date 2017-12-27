a973b1a9033e62725f10c28c9e957a7ef5ef7973
fantasygamery <30944486+fantasygamery@users.noreply.github.com>
2017-12-27 11:51:05 -0500

    Update Main.js

--- Src/Main.js
+++ Src/Main.js
@@ -6,6 +6,23 @@
   _function(_object[x],'Sup?');
   }
   }
+  this.wait=function (secs,func,ID){
+var _towait=secs;var waited=0;
+var INsT = null
+function _int(){
+    waited=++waited
+if (ID){
+document.getElementById(ID).innerHTML=_towait-waited
+}
+    if(waited >= _towait){
+try {
+func();
+} catch( __ERROR ){console.log(__ERROR.message)}
+window.clearInterval(INsT)
+}
+}
+INsT=window.setInterval(_int,1000);
+}  
   return this
   }
  window.nn=nnFunc();
