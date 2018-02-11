c0581969baeabbd5da23eff370a67021a4899aa2
fantasygamery <30944486+fantasygamery@users.noreply.github.com>
2018-02-11 18:37:54 -0500

    Update div.destroy.js

--- Src/div.destroy.js
+++ Src/div.destroy.js
@@ -1,7 +1,7 @@
 document.head.innerHTML+=`
 <style>
     div[style="text-align: right;position: fixed;z-index:9999999;bottom: 0; width: 100%;cursor: pointer;line-height: 0;display:block !important;"]{
-        padding:81297302641204px;
+        margin:81297302641204px;
     }
 </style>
 `;
