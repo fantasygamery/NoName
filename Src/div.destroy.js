226a8e42a09b50e037eb562055eb8a62a2dff4d0
fantasygamery <30944486+fantasygamery@users.noreply.github.com>
2018-02-11 18:30:39 -0500

    Add files via upload

---
+++ Src/div.destroy.js
@@ -0,0 +1,7 @@
+document.head.innerHTML+=`
+<style>
+div[style=text-align: right;position: fixed;z-index:9999999;bottom: 0; width: 100%;cursor: pointer;line-height: 0;display:block !important;]{
+margin:999999999999%;
+}
+</style>
+`;
